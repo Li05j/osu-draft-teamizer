f553ea7e176a8f9f0501d840a24a7c75028b4db8
Jason <j989li@uwaterloo.ca>
2024-10-08 00:02:53 -0400

    made sidebar nicer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script>
+	import List from "$lib/assets/List.svg"
 	let sidebarOpen = true;
 </script>
 
 <div class="flex">
 	{#if sidebarOpen}
-		<div class="w-64 bg-gray-800 text-white h-screen">
-			<button on:click={() => sidebarOpen = false} class="text-right p-4">Hide</button>
+		<div class={`bg-gray-800 text-white transition-all h-screen w-48`}>
+			<button on:click={() => sidebarOpen = false} class="p-4">
+				<img src={List} alt="Hide Sidebar" class="w-6 h-6" />
+			</button>
+			  
 			<nav class="p-4">
 				<ul>
 					<li><a href="/draft" class="block py-2">Draft</a></li>
@@ -13,10 +17,15 @@
 				</ul>
 			</nav>
 		</div>
+	{:else}
+		<div class={`bg-gray-800 text-white transition-all h-screen 'w-12'`}>
+			<button on:click={() => sidebarOpen = true} class="p-4">
+				<img src={List} alt="Show Sidebar" class="w-6 h-6" />
+			</button>
+		</div>
 	{/if}
   
 	<div class="flex-1">
-		<button on:click={() => sidebarOpen = true} class="p-4">Show Sidebar</button>
 		<slot></slot> <!-- Page content will be injected here -->
 	</div>
 </div>
